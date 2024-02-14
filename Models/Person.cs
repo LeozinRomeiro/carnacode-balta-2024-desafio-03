339dcd744a739dce88faf747e837fa9a49272561
Leonardo Romeiro <leozinwork@gmail.com>
2024-02-13 21:57:52 -0300

    Clonando repositorio do grupo

---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using Imc.Models.Enums;
+
+namespace Imc.Models
+{
+    public class Person
+    {
+/*        public Person(float height, float weight, ESex sex, bool isElderly)
+        {
+            Height = height;
+            Weight = weight;
+            Sex = sex;
+            IsElderly = isElderly;
+        }*/
+        public decimal? Height { get;  set; }
+        public decimal? Weight { get;  set; }
+        public ESex? Sex { get;  set; }
+        public bool IsElderly { get;  set; }
+    }
+}
